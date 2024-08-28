a6d8363e14deae8b0a6aa57345e8e1145c58f7b3
Mohamedj23 <mohamedj239@gmail.com>
2024-08-28 11:05:58 +0200

    Configure H2 DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO DEVICE (device_name, device_brand, creation_time) VALUES ('Archer AX21', 'TP-Link', CURRENT_TIMESTAMP());
+INSERT INTO DEVICE (device_name, device_brand, creation_time) VALUES ('AX6600', 'Huawei', CURRENT_TIMESTAMP());
+INSERT INTO DEVICE (device_name, device_brand, creation_time) VALUES ('CISCO1801', 'Cisco', CURRENT_TIMESTAMP());
+INSERT INTO DEVICE (device_name, device_brand, creation_time) VALUES ('Orbi', 'NETGEAR', CURRENT_TIMESTAMP());
